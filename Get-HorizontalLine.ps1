6667c3dd7397f18afeb983fb5d662a4885fbe7d8
kmaddox86 <kendall.maddox@gmail.com>
2014-02-12 11:36:50 -0600

    Changed formatting and cast variables
    Changed the formatting to be a bit more readable.  Also cast $character as string and $count as int.  Add validation for both parameters.

--- Get-HorizontalLine.ps1
+++ Get-HorizontalLine.ps1
@@ -1,6 +1,16 @@
-Function Get-HorizontalLine($Character = "-", $Count = 1) {
-    $width = $host.UI.RawUI.BufferSize.Width - 1
-    1..$Count | % { $Character * $width }
+Function Get-HorizontalLine
+{
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [string]$Character = '-',
+        
+        [ValidateNotNullOrEmpty()]
+        [int]$count = 1
+    )
+
+    $width = $host.UI.RawUI.BufferSize.Width -1
+    1..$count | ForEach-Object{$Character * $width}
+
 }
 
 Set-Alias -Name hr -Value Get-HorizontalLine
