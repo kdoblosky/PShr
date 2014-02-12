529fe189e2708b1b560c4519e5e53cddef57d959
unknown <dobloskyk@4f48fn1dobloskk.sqdev.sunquestinfo.com>
2014-02-12 11:13:23 -0700

    Renamed $Character to $InputString.
    On suggestion from reddit user law18, made sure $InputString is a string.
    
    Rewrote it to accommodate multi-character strings.

--- Get-HorizontalLine.ps1
+++ Get-HorizontalLine.ps1
@@ -1,6 +1,15 @@
-Function Get-HorizontalLine($Character = "-", $Count = 1) {
+Function Get-HorizontalLine([string]$InputString = "-", $Count = 1) {
+    # How long to make the hr
     $width = $host.UI.RawUI.BufferSize.Width - 1
-    1..$Count | % { $Character * $width }
+
+    # How many times to repeat $Character in full
+    $repetitions = [System.Math]::Floor($width/$InputString.Length)
+
+    # How many characters of $InputString to add to fill each line
+    $remainder = $width - ($InputString.Length * $repetitions)
+
+    # Make line(s)
+    1..$Count | % { ($InputString * $repetitions) + $InputString.Substring(0,$remainder)}
 }
 
 Set-Alias -Name hr -Value Get-HorizontalLine
